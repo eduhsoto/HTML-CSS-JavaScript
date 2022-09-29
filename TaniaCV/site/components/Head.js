ea18b96fa600f9127a4d560669c6662b5a8f4acf
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-09-28 19:46:40 -0500

    component head

---
+++ TaniaCV/site/components/Head.js
@@ -0,0 +1,52 @@
+"use strict"
+export class Head extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode: "open"});
+    }
+
+    setTitle(val){
+        return this.setAttribute('title', val);
+    }
+
+    setParagraph(val){
+        return this.setAttribute('paragraph', val);
+    }
+
+    setImage(){
+        return this.setAttribute('img', val);
+    }
+
+    static get observedAttributes() {
+        return [''];
+    }
+    
+    attributeChangedCallback(name, oldVal, newVal) {
+    }
+
+    connectedCallback(){
+        this.shadowRoot.innerHTML = this.render();
+        
+        this.$image = this.shadowRoot.querySelector('.image');
+        this.getImage();
+    }
+
+    getImage(){
+        this.$image.src = `${this.getAttribute('img')}`
+    }
+
+    disconnectedCallback(){
+    }
+
+    render(){
+        return `
+        <section class="head">
+            <img alt="profile image" class="image">
+            <div class="description">
+                <h1 class="name">${this.getAttribute('title')}</h1>
+                <p class="description">${this.getAttribute('paragraph')}</p>
+            </div>
+        </section>`
+    }
+}
