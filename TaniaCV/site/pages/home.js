de091bd05b8a0efc62d58cc14e1326e1cfa2945d
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-09-30 20:34:00 -0500

    add skills component show li lopp

--- TaniaCV/site/pages/home.js
+++ TaniaCV/site/pages/home.js
@@ -1,4 +1,5 @@
 import {Head} from "../components/Head.js";
+import {Skill} from "../components/Skill.js"
 
 export class Home extends HTMLElement{
     constructor(){
@@ -8,21 +9,42 @@ export class Home extends HTMLElement{
     connectedCallback(){
         this.innerHTML = this.render();
 
-        this.$headElement = this.querySelector('#head');
         this.generateHeadElement();
+        this.generateSkillElement();
     }
 
     generateHeadElement(){
-        let headElement = new Head();
-        this.attributesHead(headElement);
-        this.$headElement.appendChild(headElement);
+      this.$headElement = this.querySelector('#head');
+
+      let headElement = new Head();
+      this.attributesHead(headElement);
+      this.$headElement.appendChild(headElement);
     }
 
     attributesHead(component){
-        component.setTitle( `Hola! <br> Mi nombre es Tania y soy diseñadora UX`);
-        component.setParagraph("Soy estudiante de Ingeniería en Desarrollo y Gestión de Software, resido actualmente en la ciudad de Guadalajara Jalisco México.");
-        component.setImage("./assets/img/profile-tania.png")
-      }
+      component.setTitle( `Hola! <br> Mi nombre es Tania y soy diseñadora UX`);
+      component.setParagraph("Soy estudiante de Ingeniería en Desarrollo y Gestión de Software, resido actualmente en la ciudad de Guadalajara Jalisco México.");
+      component.setImage("./assets/img/profile-tania.png")
+    }
+
+    generateSkillElement(){
+      this.$skillElement = this.querySelector('#skills');
+
+      let skillSoft = new Skill();
+      let skillHard = new Skill();
+      
+      this.attributesSkill(skillSoft, skillHard);
+      this.$skillElement.appendChild(skillSoft);
+      this.$skillElement.appendChild(skillHard);
+    }
+
+    attributesSkill(skillSoft, skillHard){
+      skillSoft.setTitle("Habilidades blandas");
+      skillSoft.setLi('["Excelente comunicación oral y escrita.", "Trabajo en equipo y capacidad de liderazgo.", "Desarrollo de experiencias centradas en el ser humano.", "Capacidad de autoaprendizaje.", "Pensamiento estratégico."]');
+
+      skillHard.setTitle("Habilidades técnicas");
+      skillHard.setLi('["HTML, CSS, Bootstrap.", "Adobe XD.", "Figma.", "Invision Studio.", "Metodologías Ágiles (Manejo de tableros Scrum y Kanban).", "Design Thinking.", "Conocimiento de leyes cognitivas, creación de flujos, investigación de casos de uso y desarrollo de diseño visual."]');
+  }
 
     disconnectedCallback(){
     }
@@ -31,7 +53,23 @@ export class Home extends HTMLElement{
       return `
       <div id="head">
       </div>
-      <h2>Técnico Superior Universitario</h2>
+      <section class="about__me">
+        <div class="container">
+            <div class="about__left">
+                <h2 class="title__grade">Técnico Superior Universitario</h2>
+                <p class="title__specialization">Especialista UX / UI orientada en el ser humano.</p>
+                <p>Soy estudiante de Ingeniería en Tecnologías de la comunicación, área desarrollo y gestión de software.</p>
+                <p>Tengo confianza en mi capacidad para generar ideas, trabajar en equipo, adquirir conocimientos y crecer profesionalmente.</p>
+                <p>Mi pasión es crear y mejorar experiencias de los usuarios, pensando en el ser humano que utiliza las aplicaciones por medio de la tecnología, agilizando los procesos con diseños funcionales y atractivos.</p>
+            </div>
+
+            <div class="about__right">
+                <img src="./assets/img/ux-image.png" alt="ux image" class="ux__image">
+            </div>
+        </div>
+      </section>
+      <div id="skills">
+      </div>
       <style>
      </style>
       `
