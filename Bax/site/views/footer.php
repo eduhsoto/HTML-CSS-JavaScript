6fe5b97e9818cb0100c05ce1c4c3834cb8c55101
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-10-17 20:07:44 -0500

    header footer views

---
+++ Bax/site/views/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer__color">
+        <div class="container card___grid grid__footer">
+            <div class="flex__colum__item grid__item1">
+                <a href="" class="item__footer p__small__roboto">Cursos</a>
+                <a href="" class="item__footer p__small__roboto">Testimonios</a>
+                <a href="" class="item__footer p__small__roboto">Nosotros</a>
+                <a href="" class="item__footer p__small__roboto">Contacto</a>
+            </div>
+            
+            <img src="./assets/img/brand-logo-footer.png" alt="brand logo footer" class="brand__logo__footer">
+
+            <div class="social__footer">
+                <a href=""><img src="./assets/img/facebook-icon.png" alt="facebook icon" class="social__icon"></a>
+                <a href=""><img src="./assets/img/instagram-icon.png" alt="instagram icon" class="social__icon"></a>
+                <a href=""><img src="./assets/img/linkedin-icon.png" alt="linkedin icon" class="social__icon"></a>
+                <a href=""><img src="./assets/img/email-icon.png" alt="email icon" class="social__icon"></a>
+                <a href=""><img src="./assets/img/twitter-icon.png" alt="twitter icon" class="social__icon"></a>
+            </div>
+        </div>
+        <p class="p__small__roboto p__copyright">Copyright Inc. Bax. &#169 2022. Design by <a href="https://www.linkedin.com/in/debora-migliani/" class="p__small__roboto">Debora Migliani Diseñadora UX/UI</a></p>
+    </footer>
